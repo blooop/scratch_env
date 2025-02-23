50c6fa66ba31b96534d221152eff8d6db6b6ef8c
Austin Gregg-Smith <blooop@gmail.com>
2025-02-23 16:54:46 +0000

    add dagger example

---
+++ example/dagger_ex.py
@@ -0,0 +1,19 @@
+import sys
+
+import anyio
+import dagger
+from dagger import dag
+
+
+async def main(args: list[str]):
+    async with dagger.connection():
+        # build container with cowsay entrypoint
+        ctr = dag.container().from_("python:alpine").with_exec(["pip", "install", "cowsay"])
+
+        # run cowsay with requested message
+        result = await ctr.with_exec(["cowsay", *args]).stdout()
+
+    print(result)
+
+
+anyio.run(main, sys.argv[1:])
