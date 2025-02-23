ca19eb6e4e33f6a9d6dbe816f9f64024b2c9c001
Austin Gregg-Smith <blooop@gmail.com>
2025-02-23 17:15:24 +0000

    mini dagger example

--- example/dagger_ex.py
+++ example/dagger_ex.py
@@ -3,17 +3,21 @@ import sys
 import anyio
 import dagger
 from dagger import dag
+from shlex import split
 
 
 async def main(args: list[str]):
-    async with dagger.connection():
+    print(args)
+    config = dagger.Config(log_output=sys.stderr)
+    async with dagger.connection(config):
         # build container with cowsay entrypoint
-        ctr = dag.container().from_("python:alpine").with_exec(["pip", "install", "cowsay"])
+        ctr = dag.container().from_("python:alpine").with_exec(split("pip install cowsay"))
 
         # run cowsay with requested message
-        result = await ctr.with_exec(["cowsay", *args]).stdout()
+        result = await ctr.with_exec(["cowsay", "-t", *args]).stdout()
 
     print(result)
 
 
-anyio.run(main, sys.argv[1:])
+anyio.run(main, ["test1"])
+# anyio.run(main, sys.argv[1:])
